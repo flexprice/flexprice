81a317b39124857d68d3029d351dcaca53df0ec5
omkar sonawane <omkarsonawane622@gmail.com>
2025-07-22 12:28:48 +0530

    feat(schema): add CreditGrantApplications table with corresponding columns to the schema

--- ent/schema/creditgrantapplication.go
+++ ent/schema/creditgrantapplication.go
@@ -2,8 +2,6 @@ package schema
 
 import (
 	"entgo.io/ent"
-	"entgo.io/ent/dialect/entsql"
-	"entgo.io/ent/schema"
 	"entgo.io/ent/schema/field"
 	baseMixin "github.com/flexprice/flexprice/ent/schema/mixin"
 	"github.com/flexprice/flexprice/internal/types"
@@ -120,11 +118,4 @@ func (CreditGrantApplication) Fields() []ent.Field {
 // Edges of the CreditGrantApplication.
 func (CreditGrantApplication) Edges() []ent.Edge {
 	return nil // define if you want relationships with CreditGrant, Subscription, etc.
-}
-
-// Add Annotations method to skip migrations for this table.
-func (CreditGrantApplication) Annotations() []schema.Annotation {
-	return []schema.Annotation{
-		entsql.Skip(),
-	}
 }
