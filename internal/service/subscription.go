63e602543f107da86c777dddc1c86502868d293c
omkar sonawane <omkarsonawane622@gmail.com>
2025-08-28 18:36:34 +0530

    refactor(costsheet, subscription): replace direct time assignments with period retrieval methods for improved accuracy and maintainability

--- internal/service/subscription.go
+++ internal/service/subscription.go
@@ -966,8 +966,8 @@ func (s *subscriptionService) GetUsageBySubscription(ctx context.Context, req *d
 			PriceID:            lineItem.PriceID,
 			Meter:              meter.ToMeter(),
 			ExternalCustomerID: customer.ExternalID,
-			StartTime:          usageStartTime,
-			EndTime:            usageEndTime,
+			StartTime:          lineItem.GetPeriodStart(usageStartTime),
+			EndTime:            lineItem.GetPeriodEnd(usageEndTime),
 			Filters:            make(map[string][]string),
 		}
 
