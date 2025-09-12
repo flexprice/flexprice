180644f235b5125ac9884f46668cbe72443d5c2c
nkmishra1997 <nikhil@flexprice.io>
2025-09-11 19:41:37 -0700

    chore(settings): use json parsing to map settings value

--- internal/domain/settings/model.go
+++ internal/domain/settings/model.go
@@ -115,3 +115,125 @@ func (s *Setting) Validate() error {
 
 	return nil
 }
+
+// ConvertValue converts the setting value to the specified type T
+// This method uses JSON marshal/unmarshal to leverage existing JSON tags
+// and provides automatic type conversion and validation
+func (s *Setting) ConvertValue(target interface{}) error {
+	if s.Value == nil {
+		return ierr.NewError("setting value is nil").
+			Mark(ierr.ErrValidation)
+	}
+
+	// Marshal the map to JSON
+	jsonBytes, err := json.Marshal(s.Value)
+	if err != nil {
+		return ierr.WithError(err).
+			WithHint("failed to marshal setting value to JSON").
+			WithReportableDetails(map[string]any{
+				"key": s.Key,
+			}).
+			Mark(ierr.ErrValidation)
+	}
+
+	// Unmarshal JSON into target struct
+	err = json.Unmarshal(jsonBytes, target)
+	if err != nil {
+		return ierr.WithError(err).
+			WithHint("failed to unmarshal JSON to target type").
+			WithReportableDetails(map[string]any{
+				"key": s.Key,
+			}).
+			Mark(ierr.ErrValidation)
+	}
+
+	return nil
+}
+
+// ConvertValueWithDefaults converts the setting value to the specified type T,
+// merging with default values first to ensure all required fields are present
+func (s *Setting) ConvertValueWithDefaults(target interface{}, defaults map[string]interface{}) error {
+	if s.Value == nil {
+		return ierr.NewError("setting value is nil").
+			Mark(ierr.ErrValidation)
+	}
+
+	// Merge defaults with actual values (actual values take precedence)
+	mergedValue := make(map[string]interface{})
+
+	// First, copy defaults
+	for k, v := range defaults {
+		mergedValue[k] = v
+	}
+
+	// Then, override with actual values
+	for k, v := range s.Value {
+		mergedValue[k] = v
+	}
+
+	// Marshal the merged map to JSON
+	jsonBytes, err := json.Marshal(mergedValue)
+	if err != nil {
+		return ierr.WithError(err).
+			WithHint("failed to marshal merged setting value to JSON").
+			WithReportableDetails(map[string]any{
+				"key": s.Key,
+			}).
+			Mark(ierr.ErrValidation)
+	}
+
+	// Unmarshal JSON into target struct
+	err = json.Unmarshal(jsonBytes, target)
+	if err != nil {
+		return ierr.WithError(err).
+			WithHint("failed to unmarshal JSON to target type").
+			WithReportableDetails(map[string]any{
+				"key": s.Key,
+			}).
+			Mark(ierr.ErrValidation)
+	}
+
+	return nil
+}
+
+// ToInvoiceConfig converts the setting value to InvoiceConfig with proper defaults
+func (s *Setting) ToInvoiceConfig() (*types.InvoiceConfig, error) {
+	if s.Key != types.SettingKeyInvoiceConfig.String() {
+		return nil, ierr.NewErrorf("setting key '%s' is not an invoice config setting", s.Key).
+			WithHint("This method should only be used with invoice config settings").
+			Mark(ierr.ErrValidation)
+	}
+
+	// Get default values for invoice config
+	defaultSettings := types.GetDefaultSettings()
+	defaults := defaultSettings[types.SettingKeyInvoiceConfig].DefaultValue
+
+	var config types.InvoiceConfig
+	err := s.ConvertValueWithDefaults(&config, defaults)
+	if err != nil {
+		return nil, err
+	}
+
+	return &config, nil
+}
+
+// ToSubscriptionConfig converts the setting value to SubscriptionConfig with proper defaults
+func (s *Setting) ToSubscriptionConfig() (*types.SubscriptionConfig, error) {
+	if s.Key != types.SettingKeySubscriptionConfig.String() {
+		return nil, ierr.NewErrorf("setting key '%s' is not a subscription config setting", s.Key).
+			WithHint("This method should only be used with subscription config settings").
+			Mark(ierr.ErrValidation)
+	}
+
+	// Get default values for subscription config
+	defaultSettings := types.GetDefaultSettings()
+	defaults := defaultSettings[types.SettingKeySubscriptionConfig].DefaultValue
+
+	var config types.SubscriptionConfig
+	err := s.ConvertValueWithDefaults(&config, defaults)
+	if err != nil {
+		return nil, err
+	}
+
+	return &config, nil
+}
