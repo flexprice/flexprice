d5b5ee936f03abafa0fbab12268a73ab74680742
omkar sonawane <omkarsonawane622@gmail.com>
2025-10-18 16:45:36 +0530

    feat: Introduce PriceUnit model, repository, and filter with conversion functions

---
+++ internal/domain/priceunit/repository.go
@@ -0,0 +1,17 @@
+package priceunit
+
+import (
+	"context"
+
+	"github.com/flexprice/flexprice/internal/types"
+)
+
+type Repository interface {
+	Get(ctx context.Context, id string) (*PriceUnit, error)
+	List(ctx context.Context, filter *types.PriceUnitFilter) ([]*PriceUnit, error)
+	Create(ctx context.Context, priceUnit *PriceUnit) (*PriceUnit, error)
+	Update(ctx context.Context, priceUnit *PriceUnit) (*PriceUnit, error)
+	Delete(ctx context.Context, id string) error
+
+	GetByCode(ctx context.Context, code string) (*PriceUnit, error)
+}
