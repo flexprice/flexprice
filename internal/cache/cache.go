43bcc0aa57cb99fdb40ba36011e96c4d4846530c
omkar sonawane <omkarsonawane622@gmail.com>
2025-10-18 16:53:18 +0530

    feat: Add PriceUnit repository implementation with caching and CRUD operations

--- internal/cache/cache.go
+++ internal/cache/cache.go
@@ -64,6 +64,7 @@ const (
 	PrefixSettings                 = "settings:v1:"
 	PrefixSubscriptionLineItem     = "subscription_line_item:v1:"
 	PrefixWalletAlertThrottle      = "wallet_alert_throttle:v1:"
+	PrefixPriceUnit                = "price_unit:v1:"
 )
 
 // GenerateKey creates a cache key from a prefix and a set of parameters
