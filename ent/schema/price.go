0651693b9d90ebad41ac513c7bc973df0b341797
omkar sonawane <omkarsonawane622@gmail.com>
2025-10-18 16:05:13 +0530

    feat: Implement PriceUnit schema and integrate with Price entity

--- ent/schema/price.go
+++ ent/schema/price.go
@@ -182,6 +182,13 @@ func (Price) Fields() []ent.Field {
 			}).
 			Optional().
 			Nillable(),
+
+		field.String("price_unit_id").
+			SchemaType(map[string]string{
+				"postgres": "varchar(50)",
+			}).
+			Optional().
+			Nillable(),
 	}
 }
 
