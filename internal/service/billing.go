78457b2d47956ad3cc732bbd260dee4ea43f3684
omkar sonawane <omkarsonawane622@gmail.com>
2026-01-06 16:53:37 +0530

    refactor: simplify invoice and line item response structures by embedding invoice types and removing redundant fields

--- internal/service/billing.go
+++ internal/service/billing.go
@@ -1949,7 +1949,6 @@ func (s *billingService) CreateInvoiceRequestForCharges(
 		PeriodStart:      &periodStart,
 		PeriodEnd:        &periodEnd,
 		BillingReason:    types.InvoiceBillingReasonSubscriptionCycle,
-		EnvironmentID:    sub.EnvironmentID,
 		Metadata:         metadata,
 		LineItems:        append(result.FixedCharges, result.UsageCharges...),
 		InvoiceCoupons:   validCoupons,
