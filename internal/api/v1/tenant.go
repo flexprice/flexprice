8dc270b32338af9b01676d0dd8c3fad0f32acd14
pratham-ez <prathamkhodwe@gmail.com>
2026-02-24 13:54:48 +0530

    remove the `CreateTenant` endpoint.

--- internal/api/v1/tenant.go
+++ internal/api/v1/tenant.go
@@ -26,35 +26,6 @@ func NewTenantHandler(
 	}
 }
 
-// @Summary Create a new tenant
-// @Description Create a new tenant
-// @Tags Tenants
-// @Accept json
-// @Produce json
-// @Security ApiKeyAuth
-// @Param request body dto.CreateTenantRequest true "Create tenant request"
-// @Success 201 {object} dto.TenantResponse
-// @Failure 400 {object} ierr.ErrorResponse
-// @Failure 500 {object} ierr.ErrorResponse
-// @Router /tenants [post]
-func (h *TenantHandler) CreateTenant(c *gin.Context) {
-	var req dto.CreateTenantRequest
-	if err := c.ShouldBindJSON(&req); err != nil {
-		c.Error(ierr.WithError(err).
-			WithHint("Please check the request payload").
-			Mark(ierr.ErrValidation))
-		return
-	}
-
-	resp, err := h.service.CreateTenant(c.Request.Context(), req)
-	if err != nil {
-		c.Error(err)
-		return
-	}
-
-	c.JSON(http.StatusCreated, resp)
-}
-
 // @Summary Get tenant by ID
 // @Description Get tenant by ID
 // @Tags Tenants
