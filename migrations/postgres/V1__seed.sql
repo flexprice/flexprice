b7ce2691f204ecf0b93f5531ca942fbf77cb4191
Agrim Mittal <16553016+agrim123@users.noreply.github.com>
2026-04-22 17:05:27 +0530

    (local) Add support for local k8s deployment using kind

--- migrations/postgres/V1__seed.sql
+++ migrations/postgres/V1__seed.sql
@@ -11,6 +11,23 @@ INSERT INTO public.tenants (
     CURRENT_TIMESTAMP
 ) ON CONFLICT (id) DO NOTHING;
 
+-- Create default auth record
+INSERT INTO public.auths (
+    user_id,
+    provider,
+    token,
+    status,
+    created_at,
+    updated_at
+) VALUES (
+    '00000000-0000-0000-0000-000000000000',
+    'flexprice',
+    'sk_local_flexprice_test_key',
+    'published',
+    CURRENT_TIMESTAMP,
+    CURRENT_TIMESTAMP
+) ON CONFLICT DO NOTHING;
+
 -- Create default user
 INSERT INTO public.users (
     id,
@@ -68,6 +85,37 @@ INSERT INTO public.environments (
     'Production',
     'production',
     '00000000-0000-0000-0000-000000000000',
+    'published',
+    '00000000-0000-0000-0000-000000000000',
+    '00000000-0000-0000-0000-000000000000',
+    CURRENT_TIMESTAMP,
+    CURRENT_TIMESTAMP
+) ON CONFLICT (id) DO NOTHING;
+
+-- Create local test API key
+INSERT INTO public.secrets (
+    id,
+    tenant_id,
+    environment_id,
+    name,
+    type,
+    provider,
+    value,
+    display_id,
+    status,
+    created_by,
+    updated_by,
+    created_at,
+    updated_at
+) VALUES (
+    'sk_local_flexprice_test_key',
+    '00000000-0000-0000-0000-000000000000',
+    '00000000-0000-0000-0000-000000000000',
+    'Local Test Key',
+    'private_key',
+    'flexprice',
+    '0cfd568f22158887f8b77bc019fb245b1f14077b80936342b052985efa7de46c',
+    'sk_local_****_test_key',
     'published',
     '00000000-0000-0000-0000-000000000000',
     '00000000-0000-0000-0000-000000000000',
