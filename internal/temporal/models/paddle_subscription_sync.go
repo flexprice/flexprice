93eb1c001219adf21f05667501bdc509f2191b86
omkar273 <omkarsonawane622@gmail.com>
2026-05-20 15:10:15 +0530

    fix(paddle): add CustomerID validation and WithHint to PaddleSubscriptionSyncWorkflowInput

--- internal/temporal/models/paddle_subscription_sync.go
+++ internal/temporal/models/paddle_subscription_sync.go
@@ -12,13 +12,24 @@ type PaddleSubscriptionSyncWorkflowInput struct {
 
 func (i *PaddleSubscriptionSyncWorkflowInput) Validate() error {
 	if i.SubscriptionID == "" {
-		return ierr.NewError("subscription_id is required").Mark(ierr.ErrValidation)
+		return ierr.NewError("subscription_id is required").
+			WithHint("SubscriptionID must not be empty").
+			Mark(ierr.ErrValidation)
+	}
+	if i.CustomerID == "" {
+		return ierr.NewError("customer_id is required").
+			WithHint("CustomerID must not be empty").
+			Mark(ierr.ErrValidation)
 	}
 	if i.TenantID == "" {
-		return ierr.NewError("tenant_id is required").Mark(ierr.ErrValidation)
+		return ierr.NewError("tenant_id is required").
+			WithHint("TenantID must not be empty").
+			Mark(ierr.ErrValidation)
 	}
 	if i.EnvironmentID == "" {
-		return ierr.NewError("environment_id is required").Mark(ierr.ErrValidation)
+		return ierr.NewError("environment_id is required").
+			WithHint("EnvironmentID must not be empty").
+			Mark(ierr.ErrValidation)
 	}
 	return nil
 }
