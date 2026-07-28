81d764a2d75f490e382178388f7226d91e86a987
omkar273 <omkarsonawane622@gmail.com>
2026-07-29 02:35:47 +0530

    refactor(subscription): publish line_item.created event on addon attach

--- internal/ee/service/subscription.go
+++ internal/ee/service/subscription.go
@@ -5053,9 +5053,7 @@ func (s *subscriptionService) addAddonToSubscription(
 		return nil, err
 	}
 
-	for _, li := range lineItems {
-		s.triggerHubSpotDealSyncForLineItem(ctx, sub.ID, sub.CustomerID, li.ID, li.PriceType, models.HubSpotLineItemSyncOperationCreated)
-	}
+	s.publishLineItemEvents(ctx, sub.ID, lineItems, types.WebhookEventSubscriptionLineItemCreated)
 
 	effectiveDate := addonRequestedStart
 	for _, li := range lineItems {
