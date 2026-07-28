02e056f3405f1f94eae19163c316879a4a2e42c1
omkar273 <omkarsonawane622@gmail.com>
2026-07-29 02:33:26 +0530

    refactor(subscription): publish line_item.created event on subscription create

--- internal/ee/service/subscription.go
+++ internal/ee/service/subscription.go
@@ -609,11 +609,7 @@ func (s *subscriptionService) CreateSubscription(ctx context.Context, req dto.Cr
 		s.triggerHubSpotQuoteSyncWorkflow(ctx, result.Sub.ID, result.Customer.ID)
 		s.runPaddleSubscriptionSync(ctx, result.Sub)
 	} else {
-		for _, li := range result.Sub.LineItems {
-			if li.PriceType == types.PRICE_TYPE_FIXED {
-				s.triggerHubSpotDealSyncForLineItem(ctx, result.Sub.ID, result.Customer.ID, li.ID, li.PriceType, models.HubSpotLineItemSyncOperationCreated)
-			}
-		}
+		s.publishLineItemEvents(ctx, result.Sub.ID, result.Sub.LineItems, types.WebhookEventSubscriptionLineItemCreated)
 		s.runPaddleSubscriptionSync(ctx, result.Sub)
 	}
 	return response, nil
