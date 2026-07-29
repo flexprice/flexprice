544dea04c4c9a9ceef29e9373c1e9a0cae1a65cd
Harshit Gupta <harshit2001411@gmail.com>
2026-07-29 17:52:44 +0530

    Revert "Merge pull request #2309 from omkar273/featly/pre-expiry-credits-consumption"
    This reverts commit a9e2f8bed1e4c48253dd8c351a9ae2cfff8e1ed5, reversing
    changes made to 853a1b1a4bee5735e02ed11016704d4ae7fb8ac5.

--- internal/cache/expiry.go
+++ internal/cache/expiry.go
@@ -3,10 +3,9 @@ package cache
 import "time"
 
 const (
-	ExpiryDefaultInMemory        = 2 * time.Minute
-	ExpiryDefaultRedis           = 30 * time.Minute
-	ExpiryWalletBalance          = 30 * time.Minute
-	ExpiryWalletAlertCheck       = 1 * time.Minute
-	ExpiryPriceSyncLock          = 2 * time.Hour
-	ExpiryPrepaidCreditApplyLock = 5 * time.Minute
+	ExpiryDefaultInMemory  = 2 * time.Minute
+	ExpiryDefaultRedis     = 30 * time.Minute
+	ExpiryWalletBalance    = 30 * time.Minute
+	ExpiryWalletAlertCheck = 1 * time.Minute
+	ExpiryPriceSyncLock    = 2 * time.Hour
 )
--- internal/repository/ent/invoice_line_item.go
+++ internal/repository/ent/invoice_line_item.go
@@ -366,10 +366,6 @@ func (r *invoiceLineItemRepository) ListByInvoiceID(ctx context.Context, invoice
 			invoicelineitem.InvoiceID(invoiceID),
 			invoicelineitem.Status(string(types.StatusPublished)),
 		).
-		Order(
-			ent.Asc(invoicelineitem.FieldCreatedAt),
-			ent.Asc(invoicelineitem.FieldID),
-		).
 		All(ctx)
 
 	if err != nil {
